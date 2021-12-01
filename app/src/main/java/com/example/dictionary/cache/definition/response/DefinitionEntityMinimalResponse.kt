1d8b15d2f27f69d0485a5c04d5b7eb754c90d18a
Anand-Pushkar <anandp2051@gmail.com>
2021-12-01 16:56:41 +0530

    updated directory names

--- app/src/main/java/com/example/dictionary/cache/definition/response/DefinitionEntityMinimalResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.dictionary.cache.definition.response
-
-data class DefinitionEntityMinimalResponse(
-    val word: String,
-    val pronunciation: String,
-    val statement: String
-)
